7b1c0085f815fa2ea3c0f92565497a9484aab9a5
unknown <maazajaz1234@gmail.com>
2022-08-07 04:57:58 +0530

    finalandlast

--- app.js
+++ app.js
@@ -165,6 +165,11 @@ app.post("/login", function(req, res) {
   });
 });
 
-app.listen(process.env.PORT || 3000, function(req, res) {
-  console.log("Server started on port 3000");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function() {
+  console.log("Server is running! ^-^ ");
 });
